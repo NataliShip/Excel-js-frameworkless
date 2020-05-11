19ea1b8d21caa3e2becae2294ce5e26e1764d001
Nataliya Shipelova <natulechka_m.25@mail.ru>
2020-05-11 03:57:50 +0300

    add loaders

--- webpack.config.js
+++ webpack.config.js
@@ -8,10 +8,12 @@ module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
   entry: './index.js',
+
   output: {
     filename: 'bundle.[hash].js',
     path: path.resolve(__dirname, 'dist')
   },
+
   resolve: {
     extensions: ['.js'],
     alias: {
@@ -19,6 +21,7 @@ module.exports = {
       '@core': path.resolve(__dirname, 'src/core'),
     }
   },
+
   plugins: [
     new CleanWebpackPlugin(),
     new HTMLWebpackPlugin({
@@ -33,5 +36,28 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'bundle.[hash].css'
     })
-  ]
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 }
