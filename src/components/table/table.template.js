168c87236067a419c1e1d85daa9f2ba168a35082
Nataliya Shipelova <natulechka_m.25@mail.ru>
2020-05-22 01:41:36 +0300

    add table template

---
+++ src/components/table/table.template.js
@@ -0,0 +1,45 @@
+import { CODES } from '@core/helpers/constants'
+
+function createCell() {
+  return `
+    <div class="cell" contenteditable>A1</div>
+  `
+}
+
+function toColumn(el) {
+  return `
+    <div class='column'>${el}</div>
+  `
+}
+
+function createRow(content) {
+  return `
+    <div class='row'>
+      <div class='row-info'></div>
+      <div class='row-data'>${content}</div>
+    </div>
+  `
+}
+
+function toChar(_, index) {
+  return String.fromCharCode(CODES.A + index)
+}
+
+export function createTable(rowsCount = 20) {
+  const colsCount = CODES.Z - CODES.A + 1
+  const rows = []
+
+  const cols = new Array(colsCount)
+      .fill('')
+      .map(toChar)
+      .map(toColumn)
+      .join('')
+
+  rows.push(createRow(cols))
+
+  for (let i = 0; i < rowsCount; i += 1) {
+    rows.push(createRow())
+  }
+
+  return rows.join('')
+}
